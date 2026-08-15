005895911ccadefa6a954f2d2319513dfe7a0781
mdhs01 <58425744+mdhs01@users.noreply.github.com>
2026-08-16 05:12:11 +0700

    Export Phase 11 operational hardening helpers

--- src/wallet_screener/__init__.py
+++ src/wallet_screener/__init__.py
@@ -8,10 +8,12 @@ from .gmgn_adapter import GMGNAdapter, GmgnEndpointMap
 from .gmgn_cli import GmgnCli, GmgnCliConfig, GmgnCliError
 from .gmgn_openapi_contract import GMGNRoute, route_table
 from .gmgn_provider import GMGNLiveProvider
+from .health import HealthReport, check_health
 from .lifecycle import LifecycleResult, WalletLifecycle
 from .manual_qa import ManualQAReport, build_manual_qa_report
 from .models import ScreeningResult, TradeObservation, WalletMetrics
 from .normalized import NormalizedWallet, SchemaError
+from .operational import CircuitBreaker, RetryPolicy, with_retry
 from .paper_tracking import PaperObservation, PaperTrackSummary, PaperTracker
 from .persistence import ScreeningStore
 from .pipeline import PipelineReport, ScreeningPipeline
@@ -30,15 +32,15 @@ from .watchlist import (
 
 __all__ = [
     "ApiClient", "ApiConfig", "ApiError", "ApiResponse",
-    "ClusterReport", "CrossTokenEvidence", "FundingAwareProvider",
+    "CircuitBreaker", "ClusterReport", "CrossTokenEvidence", "FundingAwareProvider",
     "FundingObservation", "FundingVerifier", "GMGNAdapter", "GMGNLiveProvider",
     "GMGNRoute", "GmgnCli", "GmgnCliConfig", "GmgnCliError", "GmgnEndpointMap",
-    "LifecycleResult", "WalletLifecycle", "ManualQAReport", "NormalizedWallet",
+    "HealthReport", "LifecycleResult", "WalletLifecycle", "ManualQAReport", "NormalizedWallet",
     "NullProvider", "PaperObservation", "PaperTrackConfig", "PaperTrackSummary",
-    "PaperTracker", "PipelineReport", "ProviderConfig", "RevalidationSnapshot",
-    "SchemaError", "ScreeningPipeline", "ScreeningResult", "ScreeningStore",
-    "ScreenerConfig", "SolanaRpcClient", "SolscanAdapter", "TradeObservation",
-    "WalletDataProvider", "WalletLink", "WalletMetrics", "WalletScreener",
-    "WatchlistCategory", "WatchlistEntry", "WatchlistManager", "WatchlistStatus",
-    "analyze_cluster", "build_manual_qa_report", "classify_category", "route_table",
+    "PaperTracker", "PipelineReport", "ProviderConfig", "RevalidationSnapshot", "RetryPolicy",
+    "SchemaError", "ScreeningPipeline", "ScreeningResult", "ScreeningStore", "ScreenerConfig",
+    "SolanaRpcClient", "SolscanAdapter", "TradeObservation", "WalletDataProvider", "WalletLink",
+    "WalletMetrics", "WalletScreener", "WatchlistCategory", "WatchlistEntry", "WatchlistManager",
+    "WatchlistStatus", "analyze_cluster", "build_manual_qa_report", "check_health", "classify_category",
+    "route_table", "with_retry",
 ]
